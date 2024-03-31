41c48b4c836836143d711c4de305b7c9e653a201
Seif Gharres <seifgh.dev@gmail.com>
2024-03-31 15:57:15 +0100

    Update to version 2024

--- nuxt/e481064.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{370:function(n,e,t){"use strict";t.r(e);var o={props:{isDown:Boolean}},l=t(7),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
